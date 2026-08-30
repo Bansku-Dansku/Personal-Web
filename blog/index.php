14ad2430649a26b39b4afee34e78145a42224fb4
Bansku-Dansku <daniel@banskudansku.net>
2026-08-30 16:56:37 +0300

    fix: Images

--- blog/index.php
+++ blog/index.php
@@ -1,7 +1,7 @@
 <?php include '../assets/scripts/navbar.php';?>
     <div class="">
       <section id="home" class="mt-2 h-[60dvh] bg-slate-900 p-3 rounded-lg border border-slate-800">
-        <div class="bg-center bg-[url('../assets/img/blog.gif')] h-full rounded-lg flex flex-col items-center justify-center text-shadow-lg text-shadow-black">
+        <div style="background-image: url('/assets/img/blog.gif');" class="bg-center h-full rounded-lg flex flex-col items-center justify-center text-shadow-lg text-shadow-black">
           <h1 class="text-7xl">Page under construction!</h1>
           <p class="mt-2 text-xl">Blog coming when I have time!</p>
         </div>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include 'assets/scripts/navbar.php';?>
     <div class="">
       <section id="home" class="mt-2 h-[60dvh] bg-slate-900 p-3 rounded-lg border border-slate-800">
-        <div class="bg-cover bg-center bg-no-repeat bg-[url('assets/img/home-cover.jpeg')] h-full rounded-lg flex flex-col items-center justify-center text-shadow-lg text-shadow-black">
+        <div style="background-image: url('/assets/img/home-cover.jpeg');" class="bg-cover bg-center bg-no-repeat h-full rounded-lg flex flex-col items-center justify-center text-shadow-lg text-shadow-black">
           <h1 class="text-7xl">Welcome, fellow people!</h1>
           <p class="mt-2 text-xl">I am Daniel, also known as Bansku Dansku. This is my website/portfolio.</p>
         </div>
